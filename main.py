b27fb286f5ef3e37a73f822cc5415d4d43d851eb
ckubudi <ckubudi@hotmail.com>
2017-02-07 18:25:24 -0200

    Decision making function implemented

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import multiplicative_weigths as mw
+
 print("Hello World!")
+
+test_weigths = [2.5 , 40.77 , 8.23, 5.0, 4.0, 10.00]
+
+maj_mode = mw.decision_mode.Majority
+dis_mode = mw.decision_mode.Distributed
+
+print "The max weigth index is " , mw.make_decision(test_weigths, dis_mode)
+
---
+++ multiplicative_weigths.py
@@ -0,0 +1,38 @@
+from enum import Enum
+import numpy as np
+
+class decision_mode(Enum):
+    Majority = 1
+    Distributed = 2
+
+#Makes decision based on distribution criteria
+def make_distributed_decision (weigths_list):
+    weigths_sum=0
+    for weigth in weigths_list:
+        weigths_sum+=weigth
+
+    probabilities_list = []
+    for weigth in weigths_list:
+        probabilities_list.append(weigth/weigths_sum)
+
+    indexes = np.arange(0, len(weigths_list))
+    index = np.random.choice(indexes , p=probabilities_list )
+    return index
+
+# Makes the decision based on decision mode
+#
+def make_decision(weigths_list, mode):
+    index = 0
+    if(mode == decision_mode.Majority):
+        for i in range(1,len(weigths_list)):
+            if(weigths_list[index] < weigths_list[i]):
+                index=i
+    else:
+        if(mode == decision_mode.Distributed):
+            index=make_distributed_decision(weigths_list)
+
+    return index
+
+
+
+
